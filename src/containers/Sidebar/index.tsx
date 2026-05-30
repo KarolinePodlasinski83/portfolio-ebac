5df48c45df90e7c6471f1ba4d22b178aa32cfad5
karolinepodlasinski83 <karolinepodlasinski83@gmail.com>
2026-05-30 18:42:44 -0300

    Estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
 import Titulo from '../../components/Titulo';
+import Avatar from '../../components/Avatar';
+import Paragrafo from '../../components/Paragrafo';
+
+import { Descriçao, BotaoTema, SidebarContainer } from './styles';
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/karolinepodlasinski83.png" />
-    <Titulo fontSize={20}>Karoline Podlasinski</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Karoline Podlasinski</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        KarolinePodlasinski83
+      </Paragrafo>
+      <Descriçao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descriçao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 );
 
